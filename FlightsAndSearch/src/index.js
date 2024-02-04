05ec302ec4ee8988d2e082b8e2c518b348a8055a
Karthikraut <karthikraut2@gmail.com>
2024-02-04 20:14:24 +0530

    Body-Parser is Setuped and  env inside gitignore.

--- FlightsAndSearch/src/index.js
+++ FlightsAndSearch/src/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const bodyParser =require("body-parser");
+
 const {PORT} =require('./config/serverConfig');
+
 
 const setupAndStartServer = async()=>{
     //Create the Express Object
     const app =express();
     
+    app.use(bodyParser.json()); //Parse JSON encoded Body
+    app.use(bodyParser.urlencoded({extended :true})); //Parse URL-Encoded Body
+
     app.listen(PORT,()=>{
         console.log(`Server started at ${PORT}`)
       //  console.log(process.env) // remove this after you've confirmed it is working

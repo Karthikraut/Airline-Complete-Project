0c9b11cb2ea367f76d733aa6c10f6c023583479e
Karthikraut <karthikraut2@gmail.com>
2024-02-04 19:45:11 +0530

    Setup Basic .env

--- FlightsAndSearch/src/index.js
+++ FlightsAndSearch/src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const {PORT} =require('./config/serverConfig');
 
 const setupAndStartServer = async()=>{
     //Create the Express Object
     const app =express();
-    const PORT =3000;
+    
     app.listen(PORT,()=>{
         console.log(`Server started at ${PORT}`)
+      //  console.log(process.env) // remove this after you've confirmed it is working
     })
 }
 

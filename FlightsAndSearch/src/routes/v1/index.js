2bcef0ff71308d711c3b4c64c3f04162d2290b2a
Karthikraut <karthikraut2@gmail.com>
2024-03-09 23:07:32 +0530

    Added fligth-create request middleware

--- FlightsAndSearch/src/routes/v1/index.js
+++ FlightsAndSearch/src/routes/v1/index.js
@@ -1,5 +1,7 @@
 const express =require('express');
 
+const {FlightMiddlewares} =require('../../middlewares/index');
+
 const CityController  =require('../../controllers/city-contoller');
 const FlightController= require('../../controllers/flight-controller');
 const AirportController = require('../../controllers/airport-controller');
@@ -12,7 +14,10 @@ router.get('/city/:id', CityController.get);
 router.get('/city', CityController.getAll);
 router.patch('/city/:id',CityController.update);
 
-router.post('/flights', FlightController.create);
+router.post('/flights',
+    FlightMiddlewares.validateCreateFlight,
+    FlightController.create
+);
 router.get('/flights', FlightController.getAll);
 
 router.post('/airports', AirportController.create);

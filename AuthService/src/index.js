19ac97d666176e011c1459f9ba8bca3ee44b15aa
Karthikraut <karthikraut2@gmail.com>
2024-03-10 23:05:42 +0530

    Creation and Verify Token Function is Implemented.

--- AuthService/src/index.js
+++ AuthService/src/index.js
@@ -3,6 +3,8 @@ const {PORT}  = require('./config/serverConfig')
 const apiRoutes =  require('./routes/index');
 const bodyParser = require('body-parser');
 
+const userService = require('../src/services/user-service');
+
 const app = express();
 
 const prepareAndStartServer =()=>{
@@ -14,6 +16,12 @@ const prepareAndStartServer =()=>{
     
     app.listen(PORT,() =>{
         console.log(`Server Started on PORT ${PORT}`);
+        const service = new userService();
+        // const token = service.createToken({email: 'sanket@gmail.com', id: 1});
+        // console.log("New Token is : ", token);
+        const token =  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhbmtldEBnbWFpbC5jb20iLCJpZCI6MSwiaWF0IjoxNzEwMDkxNzg1LCJleHAiOjE3MTAwOTIzODV9.X91YqGaiG_y-QwotPBbk4oDH1YIQjaT-yGr6oAL3I5A';
+        const response = service.verifyToken(token);
+        console.log(response);
     })
 }
 
--- AuthService/src/services/user-service.js
+++ AuthService/src/services/user-service.js
@@ -1,5 +1,6 @@
 const UserRepository = require("../repository/user-repository");
-
+const jwt =require('jsonwebtoken')
+const {JWT_KEY} =require('../config/serverConfig')
 class UserService{
 
     constructor(){
@@ -12,6 +13,26 @@ class UserService{
             return user;
         } catch(error){
             console.log("Something Went wrong in service layer.");
+            throw error;
+        }
+    }
+
+    createToken(user){
+        try{
+            var token = jwt.sign(user,JWT_KEY, {expiresIn: 600});
+            return token;
+        } catch(error){
+            console.log("Error occurs at creating Token.");
+            throw error;
+        }
+    }
+
+    verifyToken(user){
+        try{
+            var response = jwt.verify(user,JWT_KEY);
+            return response;
+        } catch(error){
+            console.log("Error occurs at verifing token.");
             throw error;
         }
     }

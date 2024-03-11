4b5f57affada09a50f4ddeaf546949487802a371
Karthikraut <karthikraut2@gmail.com>
2024-03-11 16:13:37 +0530

    Added "roles" and "user_roles" through table.

--- AuthService/src/index.js
+++ AuthService/src/index.js
@@ -3,7 +3,10 @@ const {PORT}  = require('./config/serverConfig')
 const apiRoutes =  require('./routes/index');
 const bodyParser = require('body-parser');
 
-const userService = require('../src/services/user-service');
+const {User} =  require('./models/index');
+const {Role} = require('./models/index');
+// const userService = require('../src/services/user-service');
+const db = require('./models/index');
 
 const app = express();
 
@@ -14,14 +17,21 @@ const prepareAndStartServer =()=>{
 
     app.use('/api',apiRoutes);
     
-    app.listen(PORT,() =>{
+    app.listen(PORT,async () =>{
         console.log(`Server Started on PORT ${PORT}`);
-        const service = new userService();
+        // const service = new userService();
         // const token = service.createToken({email: 'sanket@gmail.com', id: 1});
         // console.log("New Token is : ", token);
         // const token =  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhbmtldEBnbWFpbC5jb20iLCJpZCI6MSwiaWF0IjoxNzEwMDkxNzg1LCJleHAiOjE3MTAwOTIzODV9.X91YqGaiG_y-QwotPBbk4oDH1YIQjaT-yGr6oAL3I5A';
         // const response = service.verifyToken(token);
         // console.log(response);
+        
+        if(process.env.DB_SYNC ){
+            db.sequelize.sync({alter :true});
+        }
+        const u1 = await User.findByPk(2);
+        const r1 = await Role.findByPk(2);
+        u1.addRole(r1);
     })
 }
 

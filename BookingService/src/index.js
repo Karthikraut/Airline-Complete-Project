7ee4f67807265228cf038e6756e94706d9d63050
Karthikraut <karthikraut2@gmail.com>
2024-04-04 20:43:41 +0530

    Booking Service:- Added Booking Model

--- BookingService/src/index.js
+++ BookingService/src/index.js
@@ -1,9 +1,14 @@
 const express =require('express');
 const app = express();
-const PORT =3002
+const PORT =3002;
+const db = require('./models/index');
+
 const setupAndStartServer = () =>{
     app.listen(PORT,()=>{
         console.log(`Server started at Port ${PORT}`)
+        if(process.env.DB_SYNC){
+            db.Sequelize.sync({alter:true});
+        }
     })
 } 
 setupAndStartServer();
